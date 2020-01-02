2f2d5a7d7b10bb9d91920cb9dad24e2192d8b6cf
iamaris <4899284+iamaris@users.noreply.github.com>
2020-01-02 21:46:49 +0000

    :≈≈new

--- cheat.py
+++ cheat.py
@@ -1,11 +1,19 @@
 import uuid
 import os
+import datetime
+
+datetime.datetime.today().weekday()
+
 
 filename = str(uuid.uuid4())
 
-with open(filename, 'w') as file:
-    file.write(filename)
 
-os.system('git add *')
-os.system('git commit -m {0}'.format(filename))
-os.system('git push origin master')
+if 6 > datetime.datetime.today().weekday() > 0: 
+    for i in random.randint(1, 15):
+    	filename = str(uuid.uuid4())
+    	with open(filename, 'w') as file:
+        	file.write(filename)
+
+    	os.system('git add *')
+    	os.system('git commit -m {0}'.format(filename))
+    	os.system('git push origin master')
